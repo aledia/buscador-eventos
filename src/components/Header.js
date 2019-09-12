77a2013a632c21c6e9d8ef9cc18a8ce367ce743f
aledia <alediaortega@gmail.com>
2019-09-12 13:15:30 +0200

    cambios

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+
+const Header = () => (
+    <header className="uk-margin" uk-margin="true">
+        <h1 className="uk-text-center">
+            Eventos con eventbrite api
+        </h1>
+    </header>
+);
+export default Header;
